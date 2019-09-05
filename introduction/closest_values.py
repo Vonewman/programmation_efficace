cca54d56a3a1eab8c4600d95ce5355ca8f845b74
Abdoulaye Diallo <vonewman7@gmail.com>
2019-09-05 21:56:04 +0000

    add closest_values.py

---
+++ introduction/closest_values.py
@@ -0,0 +1,8 @@
+def closest_values(L):
+    """
+    How to sort an array
+    """
+    assert len(L) >= 2
+    L.sort()
+    valmin, argmin = min((L[i] - L[i-1], i) for i in range(1, len(L)))
+    return L[argmin - 1], L[argmin]
